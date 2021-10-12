f0f7996278b8a99897ca09e598349d65a33e1904
BaiCaiDaoDao <90268894+BaiCaiDaoDao@users.noreply.github.com>
2021-10-13 07:40:36 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hot-fix!");
         System.out.println("hot-fix!2");
         System.out.println("master-test");
+        System.out.println("xxyy-test");
 
     }
 }
